18beb96892abe04a9fa73c6646f93e054fe431f1
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-08-07 21:54:22 -0300

    Configuração do Routes

---
+++ Touring/src/pages/Login/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+
+const Login = () => {
+  return (
+    <>
+      <View>
+        <Text>
+          Caio nao sabe de nd e o lucas nao sabe nd
+          </Text>
+      </View>
+    </>
+  );
+}
+export default Login;
---
+++ Touring/src/routes.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './pages/Login';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
