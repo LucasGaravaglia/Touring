9d963e6842292caf9acd1e4ff5bb77da5d3914fd
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-08-07 22:29:20 -0300

    Estruturação da pasta page

---
+++ Touring/src/pages/client/FindTouristSpot/index.tsx
@@ -0,0 +1,12 @@
+mport React from 'react;
+import { View, Text } from 'react-native;
+
+const PageNameHere = () => {
+  return(
+    <View>
+      <Text>PageNameHere</Text>
+    </View>
+  );
+}
+
+export default PageNameHere;
---
+++ Touring/src/pages/client/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react;
+import { View, Text } from 'react-native;
+
+const PageNameHere = () => {
+  return(
+    <View>
+      <Text>PageNameHere</Text>
+    </View>
+  );
+}
+
+export default PageNameHere;
